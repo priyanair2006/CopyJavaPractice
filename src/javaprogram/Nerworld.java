387c8be827bb1c2a6a36bca49b4bfaa2c234def0
priyanair2006 <priyanair2006@gmail.com>
2020-12-13 19:31:17 +0530

    feature branch commit

--- src/javaprogram/Nerworld.java
+++ src/javaprogram/Nerworld.java
@@ -4,6 +4,8 @@ public class Nerworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("new feature added");
 
 	}
 
--- src/javaprogram/fourth.java
+++ src/javaprogram/fourth.java
@@ -1,1 +1,13 @@
+package javaprogram;
+
+public class fourth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("new feature1 added");
+
+	}
+
+}
 
